1742c366b2ba2b1ab8cfc2b351bc1d806e82ec7f
R. C. Junior <cj.junior1995@hotmail.com>
2014-11-24 17:06:36 -0200

    Inclusão de alguns arquivos
    Inclusão de alguns arquivos

---
+++ financeiro.py
@@ -0,0 +1,16 @@
+__author__ = 'R. C. Junior'
+
+class financeiro():
+
+    def __init__(self):
+        self.fin_codigo = 0
+        self.fin_numero = 0
+        self.fin_emissao = ""
+        self.fin_vencimento = ""
+        self.fin_pagamento = ""
+        self.fin_valor = 0
+        self.fin_juros = 0
+        self.fin_desconto = 0
+        self.fin_historico = ""
+
+    def __init__(self):
